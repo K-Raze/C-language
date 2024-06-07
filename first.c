5ec8d01eccfc4d66eaf37857fd1d1efda04e0105
K-Raze <dhamijakartik2005@gmail.com>
2024-06-07 23:25:59 +0530

    i made a simple adding tool

--- first.c
+++ first.c
@@ -2,6 +2,15 @@
 
 int main()
 {
-    printf("Hello i am learning c with harry");
+    int a = 4;
+    float b = 6.6;
+    char c = 'a';
+    int d = 9;
+    int e = 22 + 43;
+    printf("the value of a is %d \n", a);
+    printf("the value of c is %c \n", c);
+    printf("the value of b is %f \n ", b);
+    printf("the  addition of a and d is %d \n", a + d);
+    printf("this is e %d \n", e);
     return 0;
 }
---
+++ input.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("write a number  \n");
+    scanf("%d", &a);
+    printf("write an another number \n");
+    scanf("%d", &b);
+    printf("this is the sum of both the number %d \n", a + b);
+
+    return 0;
+}
