1158321a9410f21dbd65640caf77782497d26005
K-Raze <dhamijakartik2005@gmail.com>
2024-06-08 12:47:32 +0530

    converted celisus to fahrenheit in c

--- temperature.c
+++ temperature.c
@@ -2,5 +2,11 @@
 
 int main()
 {
-    float return 0;
+    float c, f;
+    printf("write the temperature in celsius to convert it into fahrenheit \n");
+    scanf("%f", &c);
+    f = (c * 9 / 5) + 32;
+    printf("this is the temeperature in fahrenheit %f", f);
+
+    return 0;
 }
