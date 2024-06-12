74b650cd0be8118982e8a65b0a89d1a41f62a12e
K-Raze <dhamijakartik2005@gmail.com>
2024-06-13 00:31:53 +0530

    using functions in c

---
+++ func_in_func.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void goodmorning();
+void goodafternoon();
+void goodnight();
+int main()
+{
+    goodmorning();
+    return 0;
+}
+void goodmorning()
+{
+    printf("good morning \n");
+    goodafternoon();
+}
+
+void goodafternoon()
+{
+    printf("good afternoon \n");
+    goodnight();
+}
+
+void goodnight()
+{
+    printf("good night \n");
+}
