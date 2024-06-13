1fb636d1772aae8bc1f9f7554284445d69c7045a
K-Raze <dhamijakartik2005@gmail.com>
2024-06-14 00:23:03 +0530

    pattern using recursions

--- pattern2.c
+++ pattern2.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
-
 void printpattern(int n);
-
 int main()
 {
-    int n;
+    int a;
     printf("write a number ,how many lines you want to print \n");
-    scanf("%d", &n);
-
-    printpattern(n);
+    scanf("%d", &a);
+    printpattern(a);
     return 0;
 }
-
 void printpattern(int n)
 {
     if (n == 1)
@@ -20,7 +16,7 @@ void printpattern(int n)
         return;
     }
     printpattern(n - 1);
-    for (int i = 0; i < (2 * n - 1); i++)
+    for (int i = 0; i <= n; i++)
     {
         printf("*");
     }
